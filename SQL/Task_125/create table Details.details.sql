1fb40e235452d2be1e7236071bafb56d60968d5c
danilz <danilzz@mail.ru>
2015-12-22 11:52:28 +0500

    ПЕ-22Б Егоров Кирилл Создание схемы и таблицы детали

---
+++ SQL/Task_125/create table Details.details.sql
@@ -0,0 +1,14 @@
+﻿-- Создание таблицы "Details.details"
+create table Details.details
+(
+	details int identity(1,1) not null,
+	name varchar(64) not null,
+	release_date date null,
+	width float,
+	height float,
+	lenght float,
+	mass float,
+	details_prefix int not null,
+	constraint PK__details primary key (details),
+	constraint FK__details__details_prefix foreign key (details_prefix) references Details.details_prefix
+)
---
+++ SQL/Task_125/create table Details.details_prefix.sql
@@ -0,0 +1,14 @@
+﻿-- Создание таблицы для префиксов деталей
+create table Details.details_prefix
+(
+	details_prefix int identity(1,1) not null,
+	code varchar(128) not null,
+	mem nvarchar(512) null,
+	constraint PK__detail_prefix primary key (details_prefix)
+)
+
+-- Создание основных префиксов.
+insert into Details.details_prefix (code, mem)
+values
+	('Wood', 'Дерево.'),
+	('Stone', 'Камень.')
