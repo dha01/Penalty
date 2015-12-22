e6d70c7af4686ec38b4aab3ded4817e9a8a3c7c8
EgorovKirill <d4bit@yandex.ru>
2015-12-22 20:49:48 +0500

    ПЕ-22Б Создание таблицы в бд, репозитория, сервиса и пользовательского интерфейса для сущности "Деталь".

---
+++ IS.Model/IS.Model/Item/Details/DetailsItem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace IS.Model.Item.Details
+{
+    /// <summary>
+    /// Класс для хранения данных детали.
+    /// </summary>
+    public class DetailsItem
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Дата выпуска.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Ширина.
+        /// </summary>
+        public float Width { get; set; }
+
+        /// <summary>
+        /// Высота.
+        /// </summary>
+        public float Height { get; set; }
+
+        /// <summary>
+        /// Длинна.
+        /// </summary>
+        public float Lenght { get; set; }
+
+
+        /// <summary>
+        /// Идентификатор материала.
+        /// </summary>
+        public DetailsMaterial Material { get; set; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Mem { get; set; }
+
+    }
+
+    /// <summary>
+    /// Материалы деталей.
+    /// </summary>
+    public enum DetailsMaterial
+    {
+        /// <summary>
+        /// Дерево.
+        /// </summary>
+        Wood,
+        /// <summary>
+        /// Камень.
+        /// </summary>
+        Stone
+    }
+}
---
+++ IS.Model/IS.Model/Service/DetailsService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IS.Model.Item.Details;
+using IS.Model.Repository.Details;
+
+namespace IS.Model.Service
+{
+    /// <summary>
+    /// Сервис для работы с Деталями.
+    /// </summary>
+    public class DetailsService
+    {
+        #region Fields
+        /// <summary>
+        /// Репозиторий деталей.
+        /// </summary>
+        private IDetailsRepository _detailsRepository;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор без параметров.
+        /// </summary>
+        public DetailsService()
+        {
+            _detailsRepository = new DetailsRepository();
+        }
+
+        /// <summary>
+        /// Конструктор класс.
+        /// </summary>
+        /// <param name="details_repository">Интерфейс репозитория деталей.</param>
+        public DetailsService(IDetailsRepository details_repository)
+        {
+            _detailsRepository = details_repository;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Получает деталь по идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        /// <returns>Деталь.</returns>
+        public DetailsItem GetById(int id)
+        {
+            return _detailsRepository.Get(id);
+        }
+
+        /// <summary>
+        /// Создает деталь.
+        /// </summary>
+        /// <param name="details">Деталь.</param>
+        /// <returns>Идентификатор созданной детали.</returns>
+        public int Create(DetailsItem details)
+        {
+            if (string.IsNullOrEmpty(details.Name))
+            {
+                throw new Exception("Поле 'Mem' не должно быть пустым.");
+            }
+
+            return _detailsRepository.Create(details);
+        }
+
+        /// <summary>
+        /// Измененяет данные детали.
+        /// </summary>
+        /// <param name="details">Деталь.</param>
+        public void Update(DetailsItem details)
+        {
+            if (string.IsNullOrEmpty(details.Name))
+            {
+                throw new Exception("Поле 'Mem' не должно быть пустым.");
+            }
+
+            if (GetById(details.Id) == null)
+            {
+                throw new Exception("Деталь не найден.");
+            }
+
+            _detailsRepository.Update(details);
+        }
+
+        /// <summary>
+        /// Удаляет деталь.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        public void Delete(int id)
+        {
+            _detailsRepository.Delete(id);
+        }
+
+        /// <summary>
+        /// Получает список задач.
+        /// </summary>
+        public List<DetailsItem> GetList()
+        {
+            return _detailsRepository.GetList();
+        }
+
+        #endregion
+    }
+}
