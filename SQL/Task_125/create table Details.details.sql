8280b0569a62892aed47a75199c558ec4c7d4748
EgorovKirill <d4bit@yandex.ru>
2015-12-24 01:28:10 +0500

    ПЕ-22Б Егоров Кирилл Исправил запрос

--- SQL/Task_125/create table Details.details.sql
+++ SQL/Task_125/create table Details.details.sql
@@ -16,4 +16,3 @@ insert into Details.details(name,release_date,width,height,lenght,mass,material)
 	values
 			('detail1','2015-01-01', 1000, 1000, 1000, 500, 'Wood'),
 			('detail2','2015-04-01', 2000, 3000, 1000, 1500, 'Glass')
-
