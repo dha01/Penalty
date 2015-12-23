4a636dfb1d32c665b58fa218e7f52d360e4afa8f
EgorovKirill <d4bit@yandex.ru>
2015-12-24 00:03:05 +0500

    ПЕ-22Б Егоров Кирилл Исправлен запрос Details.details.sql

--- SQL/Task_125/create table Details.details.sql
+++ SQL/Task_125/create table Details.details.sql
@@ -12,7 +12,7 @@ create table Details.details
 	constraint PK__details primary key (details)
 )
 -- Добавляние записей в таблицу.
-insert into Details.details(name,release_date,width,height,lenght,material)
+insert into Details.details(name,release_date,width,height,lenght,mass,material)
 	values
 			('detail1','2015-01-01', 1000, 1000, 1000, 500, 'Wood'),
 			('detail2','2015-04-01', 2000, 3000, 1000, 1500, 'Glass')
